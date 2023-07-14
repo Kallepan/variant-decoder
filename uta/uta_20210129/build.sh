7e64726e524307b0dde936fcdf6cdfa6134747b2
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-07-14 19:33:46 +0000

    feature: removed proxy settings

--- uta/uta_20210129/build.sh
+++ uta/uta_20210129/build.sh
@@ -8,7 +8,7 @@ export $(grep -v '^#' .env | xargs)
 echo $DOCKER_REGISTRY_PASSWORD | docker login --username $DOCKER_REGISTRY_USERNAME --password-stdin
 
 # Build and push uta
-docker build --no-cache -f Dockerfile --build-arg uta_version=${UTA_VERSION} --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTPS_PROXY -t kallepan/uta:${UTA_VERSION} .
+docker build --no-cache -f Dockerfile --build-arg uta_version=${UTA_VERSION} -t kallepan/uta:${UTA_VERSION} .
 docker tag kallepan/uta:${UTA_VERSION} kallepan/uta:latest
 docker push kallepan/uta:${UTA_VERSION}
 
